3da1c7c6d2274245a303636b0e79b8db431700c6
Sergio Salvago Medina <salvagomedinasergio@gmail.com>
2023-11-13 20:00:46 +0100

    Updateando curso-formulario

---
+++ curso-formulario/js/animacion.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+    document.getElementsByClassName("c1")[0].classList.add("chincheta1");
+    document.getElementsByClassName("a1")[0].classList.add("animArticle1");
+    document.getElementsByClassName("c2")[0].classList.add("chincheta2");
+    document.getElementsByClassName("c3")[0].classList.add("chincheta3");
+    document.getElementsByClassName("a2")[0].classList.add("animArticle3");
+}
